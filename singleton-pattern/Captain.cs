aa0a8d3a8abd68eda96930dc2da3ff5d80ea7207
Florescu Cristian <qflorescucristian@gmail.com>
2023-09-07 11:29:22 +0300

    Added singleton design pattern.

---
+++ singleton-pattern/Captain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton_pattern
+{
+    public class Captain
+    {
+        private static Captain? _captain = null;
+        private static readonly object _lock = new object();
+
+        private Captain()
+        {
+            Console.WriteLine("You captain has been selected!");
+        }
+
+        public static Captain GetCaptain()
+        {
+            lock (_lock)
+            {
+                if (_captain == null)
+                {
+                    _captain = new Captain();
+                }
+                else
+                {
+                    Console.WriteLine("You already have a captain!");
+                    Console.WriteLine("Send him for the toss.");
+                }
+                return _captain;
+            }
+        }
+    }
+}
---
+++ singleton-pattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using singleton_pattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Captain captain1 = Captain.GetCaptain();
+
+        Captain captain2 = Captain.GetCaptain();
+    }
+}
